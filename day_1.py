8d4d6e8872a05f0f83cf0643803ad1ca6cd14fbb
Christian Nwachukwu <christiannwachukwu754@gmail.com>
2024-11-05 18:16:13 -0600

    Add the sum of two numbers

--- day_1.py
+++ day_1.py
@@ -1,2 +1,20 @@
-print("Hello, world!")
+def add(a: int, b: int) -> int:
+    """
+    Adds the sum of two numbers.
 
+    Args:
+        a (int): The first number
+        b (int): The second number
+
+    Returns:
+        int: The sum of `a` and `b`
+    """
+    return a + b
+
+
+if __name__ == '__main__':
+    a: int = 4
+    b: int = 8
+
+    result: int = add(a, b)
+    print(result)
